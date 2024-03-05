30e261386dce221bf0db54d0aea20febbf4cee23
채상희 <chaesanghui142@gmail.com>
2024-03-05 10:49:31 +0900

    ex114

---
+++ Ex14.java
@@ -0,0 +1,12 @@
+public class Ex14 {
+    public int[] solution(String[] strlist) {
+        int[] answer = {};
+        return answer;
+    }
+
+    public static void main(String[] args) {
+    String[] strlist = {"abc", "de", "fghij"};
+        System.out.println(strlist[2].length());
+    }
+    
+}
--- index.java
+++ index.java
@@ -1,2 +1,20 @@
 public class index {
+    public static int[] solution(int[] num_list) {
+        int length = num_list.length;
+        int[] answer = new int[length];
+        int index = 0;
+        for (int i = num_list.length - 1; i >= 0; i--) {
+            answer[index] = num_list[i];
+            index++;
+        }
+        return answer;
+    }
+    public static void main(String[] args) {
+        int[] num_list = {4,5,6};
+        int[] answer = solution(num_list);
+        for (int i : answer) {
+            System.out.println(i);
+        }
+
+    }
 }
