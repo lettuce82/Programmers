312b18b4d517f0edc1cd5abf1779b17ea00eb3b2
채상희 <chaesanghui142@gmail.com>
2024-03-20 09:01:22 +0900

    Ex23 -ing

---
+++ Ex23.java
@@ -0,0 +1,10 @@
+/*
+ * 정수 배열 numbers와 정수 num1, num2가 매개변수로 주어질 때,
+ * numbers의 num1번 째 인덱스부터 num2번째 인덱스까지 자른 정수 배열을 return 하도록 solution 함수를 완성해보세요.
+ */
+public class Ex23 {
+    public static int[] solution(int[] numbers, int num1, int num2) {
+        int[] answer = {};
+        return answer;
+    }
+}
